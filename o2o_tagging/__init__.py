7b96f0031940094a874e2d6dcd93e210fbb45306
Alejandro Varas <alej0varas@gmail.com>
2013-01-29 12:24:34 -0800

    Version bump

--- o2o_tagging/__init__.py
+++ o2o_tagging/__init__.py
@@ -1,6 +1,7 @@
 """Use this app to tag objects like you tag your friends on facebook,
-but using any objects as the tagger and the tagged.
+but using any objects as the tagger and the tagged. Think of a `User`
+tagging a `Friend` in a `Photo` all of them being different models.
 
 """
 
-__version__ = '0.0.1'
+__version__ = '0.1.0'
